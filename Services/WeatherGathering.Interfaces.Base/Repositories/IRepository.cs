df4e06313270e745a47184d00ec06561482810c1
elizabeth-tech <elizabeth-tech@gmail.com>
2021-07-23 12:54:05 +0700

    Доработка интерфейса репозитория

--- Services/WeatherGathering.Interfaces.Base/Repositories/IRepository.cs
+++ Services/WeatherGathering.Interfaces.Base/Repositories/IRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using WeatherGathering.Interfaces.Base.Entities;
 
@@ -7,41 +8,48 @@ namespace WeatherGathering.Interfaces.Base.Repositories
 {
     public interface IRepository<T> where T : IEntity
     {
-        Task<bool> ExistId(int id);
+        // Есть ли сущность с указанным Id
+        Task<bool> ExistId(int id, CancellationToken cancel = default);
 
-        Task<bool> Exist(T item);
+        // Проверка сущности по Id
+        Task<bool> Exist(T item, CancellationToken cancel = default);
 
-        Task<int> GetCount();
+        // Сколько сущностей в репозитории
+        Task<int> GetCount(CancellationToken cancel = default);
 
-        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetAll(CancellationToken cancel = default);
 
-        Task<IEnumerable<T>> GetSkip(int skip, int count);
+        // Возвращаем перечисление, пропуская какое-то кол-во значений
+        Task<IEnumerable<T>> GetSkip(int skip, int count, CancellationToken cancel = default);
 
-        //Task<> GetPage(int pageIndex, int pageSize);
+        // Постраничное разбиение
+        Task<IPage<T>> GetPage(int pageIndex, int pageSize, CancellationToken cancel = default);
 
         //async Task<T> GetById(int id) => (await GetAll()).FirstOrDefault(item => item.Id == id);
 
-        Task<T> GetById(int id);
+        Task<T> GetById(int id, CancellationToken cancel = default);
 
-        Task<T> Add(T item);
+        Task<T> Add(T item, CancellationToken cancel = default);
 
-        Task<T> Update(T item);
+        Task<T> Update(T item, CancellationToken cancel = default);
 
-        Task<T> Delete(T item);
+        Task<T> Delete(T item, CancellationToken cancel = default);
 
-        Task<T> DeleteById(int id);
+        Task<T> DeleteById(int id, CancellationToken cancel = default);
     }
 
     public interface IPage<T>
     {
         IEnumerable<T> Items { get; }
 
+        // Сколько элементов во всей выборке
         int TotalCount { get; }
 
         int PageIndex { get; }
 
         int PageSize { get; }
 
+        // Полное кол-во страниц
         int TotalPagesCount => (int)Math.Ceiling((double)TotalCount / PageSize);
     }
 }
