c75e6e53b0c77bba6e80e8d84c94ab57698145f5
elizabeth-tech <elizabeth-tech@gmail.com>
2021-07-27 13:31:08 +0700

    Интерфейс репозитория для именованных сущностей

---
+++ Services/WeatherGathering.Interfaces.Base/Repositories/INamedEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WeatherGathering.Interfaces.Base.Entities;
+
+namespace WeatherGathering.Interfaces.Base.Repositories
+{
+    public interface INamedEntityRepository<T> : IRepository<T> where T : INamedEntity
+    {
+        Task<T> ExistName(string name, CancellationToken cancel = default);
+
+        Task<T> GetByName(string name, CancellationToken cancel = default);
+
+        Task<T> DeleteByName(string name, CancellationToken cancel = default);
+    }
+    
+}
