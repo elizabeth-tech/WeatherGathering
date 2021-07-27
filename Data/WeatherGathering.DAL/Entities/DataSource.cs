dac77c16f848c696ceb4dbe54609c2f103e8a4a0
elizabeth-tech <elizabeth-tech@gmail.com>
2021-07-27 14:10:04 +0700

    Добавлен индекс в таблицу DataSource со свойством Name и требованием к уникальности

--- Data/WeatherGathering.DAL/Entities/DataSource.cs
+++ Data/WeatherGathering.DAL/Entities/DataSource.cs
@@ -1,8 +1,10 @@
-﻿using WeatherGathering.DAL.Entities.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherGathering.DAL.Entities.Base;
 
 namespace WeatherGathering.DAL
 {
     // Класс источника данных
+    [Index(nameof(Name), IsUnique = true)] // говорим EF, что для данной сущности нужна индексация по колонке Name с требованием уникальности
     public class DataSource : NamedEntity
     {
         public string Description { get; set; }
