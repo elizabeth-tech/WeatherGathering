4c906905acffc862183659f37d4b2a8fc0f35f63
elizabeth-tech <elizabeth-tech@gmail.com>
2021-07-27 17:05:12 +0700

    WebAPI для доступа к репозиторию источников данных

---
+++ Services/WeatherGathering.API/Controllers/DataSourcesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherGathering.DAL;
+using WeatherGathering.Interfaces.Base.Repositories;
+
+namespace WeatherGathering.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> repository;
+
+        public DataSourcesController(IRepository<DataSource> repository) => this.repository = repository;
+
+        [HttpGet("count")] // localhost:44301/api/DataSources/count
+        public async Task<IActionResult> GetItemsCount() => Ok(await repository.GetCount());
+    }
+}
